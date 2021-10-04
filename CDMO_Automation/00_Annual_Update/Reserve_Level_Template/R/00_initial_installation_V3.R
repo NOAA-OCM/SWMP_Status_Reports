950833b5091e596face4050003cd46218fd869ef
Dave Eslinger <dave.eslinger@fnoaa.gov>
2021-10-04 11:35:18 -0400

    Update 00_initial_installation_V3.R

--- CDMO_Automation/00_Annual_Update/Reserve_Level_Template/R/00_initial_installation_V3.R
+++ CDMO_Automation/00_Annual_Update/Reserve_Level_Template/R/00_initial_installation_V3.R
@@ -1,3 +1,23 @@
+using <- function(...) {
+  libs <- unlist(list(...))
+  req <- unlist(lapply(libs,require,character.only=TRUE))
+  need <-libs[req==FALSE]
+  n <-length(need)
+  if(n > 0){
+    libsmsg <-if(n > 2) paste(paste(need[1:(n-1)], collapse=", "),",",sep="") else need[1]
+    print(libsmsg)
+    if(n > 1){
+      libsmsg <-paste(libsmsg," and ", need[n],sep="")
+    }
+    libsmsg <- paste("The following packages could not be found: ",libsmsg,
+                     "\n\r\n\rInstall missing packages?", collapse="")
+    if(winDialog(type = c("yesno"), libsmsg)=="YES"){       
+      install.packages(need, repos = "http://cran.us.r-project.org")
+      lapply(need, require, character.only=TRUE)
+    }
+  }
+}
+
 pkg_data_wrangle <- c('broom', 'lubridate', 'openxlsx', 'readxl')
 
 pkg_analysis <- c('EnvStats', 'ggthemes', 'RColorBrewer')
@@ -8,4 +28,4 @@ pkg_reporting <- c('flextable', 'officer')
 
 pkg_swmp <- c('SWMPr', 'SWMPrExtension', 'devtools')
 
-install.packages(c(pkg_data_wrangle, pkg_analysis, pkg_map, pkg_reporting, pkg_swmp), repos = "http://cran.us.r-project.org")
+using(c(pkg_data_wrangle, pkg_analysis, pkg_map, pkg_reporting, pkg_swmp))
