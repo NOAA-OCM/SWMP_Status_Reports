763854b2baae027ac5ac94de0f855f6f1806bc18
Dave Eslinger <dave.eslinger@fnoaa.gov>
2022-05-31 13:19:25 -0400

    Update the html page for downloading 2022 runs of 2021 data

--- CDMO_Automation/00_Additional_Materials/R/create_download_html.R
+++ CDMO_Automation/00_Additional_Materials/R/create_download_html.R
@@ -7,8 +7,17 @@ reserves <- read.csv(("../NERRS_sites.csv"))
 
 ofile <- ("./results/index.html")
 
-header <- "<!DOCTYPE html>  <body> 
-<h1>SWMP Status Report runs for 2021</h1>"
+header <- '<!DOCTYPE html>  <body> 
+<h1>SWMP Status Report runs for 2021</h1>
+
+<h2>These reports are run with data downloaded from CDMO on 04/02/2022</h2>
+
+<p>For the most recent data, always download zip-formatted data from
+<a href="https://cdmo.baruch.sc.edu/aqs/zips.cfm">CDMO</a></p>
+
+<p>If you are having issues or run into problems downloading these files, 
+contact Kirk.Waters@noaa.gov.  If you have R questions, you can still ask
+Dave Eslinger at dave.eslinger01@gmail.com.</p>'
 footer <- " </body></html>"
 
 write(header, file = ofile)
